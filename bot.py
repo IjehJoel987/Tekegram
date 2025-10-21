1bce09e0e79dab403908ae1b6400379125773810
Ijeh Joel <cijeh.2404322@stu.cu.edu.ng>
2025-10-22 00:15:48 +0100

    Update Tekegram

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("telegram").setLevel(logging.WARNING)
 
 BOT_TOKEN = "8419540148:AAFecDSbpcxB2-cys1bhuCZWV2qrWxEPpRY"
-CLIENT_ID = 6340039582
+CLIENT_ID = 7160317469
 NIGERIA_TZ = timezone(timedelta(hours=1))
 DATA_FILE = "teeshoot_data.json"
 # Admin management
@@ -2306,3 +2306,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
